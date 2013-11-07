042dd31a647eaee10630a87d15d62874b680b48a
Jerry Shea <jerry.shea@gmail.com>
2013-11-08 08:53:57 +1100

    rename file

--- download.sh
+++ download.sh
@@ -9,7 +9,7 @@ python download.py "$@"
 #echo $?
 
 # grep JSON from html and make it well-formed
-cat store/*.html | grep -E '\"startDate\"|\"endDate\"|\"value\"' | ./to_json.sh > store/all.json
+cat store/*.html | grep -E '\"startDate\"|\"endDate\"|\"value\"' | ./eahtml_to_json.sh > store/all.json
 
 # json->csv and grep out any null values
 python json_to_csv.py store/all.json | grep -v ',[^0-9]$' > store/all.csv
